2a9f3b9004877aeefd1cee781515920534280b34
mandytrex <amandatrexler1012@gmail.com>
2015-01-23 09:48:55 -0500

    removed field from form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ActiveRecord::Base
 	validates_presence_of :first_name, :email, :age, :avatar
 	validates :username, presence: true, uniqueness: true
 
-	attr_accessible :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	has_secure_password
 	
